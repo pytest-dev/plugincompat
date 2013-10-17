639c303efd96eb51b156d3b77982dea4b723fddd
Bruno Oliveira <nicoddemus@gmail.com>
2013-10-16 22:37:43 -0300

    ported run.py to Python 3

--- run.py
+++ run.py
@@ -1,13 +1,19 @@
-from __future__ import with_statement
-import glob
+from __future__ import print_function, with_statement, division
+
 import os
 import sys
 import tarfile
-import urllib
-import xmlrpclib
 from zipfile import ZipFile
 
-import simplejson
+#===================================================================================================
+# py2x3 compatibility
+#===================================================================================================
+if sys.version_info[0] == 3:
+    from xmlrpc.client import ServerProxy
+    from urllib.request import urlretrieve
+else:
+    from xmlrpclib import ServerProxy
+    from urllib import urlretrieve
 
 
 #===================================================================================================
@@ -33,7 +39,7 @@ def download_package(client, name, version):
         basename = os.path.basename(url_data['url'])
         found_dists.append(url_data['packagetype'])
         if url_data['packagetype'] == 'sdist':
-            urllib.urlretrieve(url_data['url'], basename)
+            urlretrieve(url_data['url'], basename)
             return basename
         
     assert 'could not found a source dist: %r' % found_dists
@@ -80,7 +86,7 @@ def run_tox(directory):
 # main
 #===================================================================================================
 def main():                    
-    client = xmlrpclib.ServerProxy('https://pypi.python.org/pypi')
+    client = ServerProxy('https://pypi.python.org/pypi')
     
     # only one package so we can quickly test the system
     #plugins = iter_plugins(client) 
@@ -91,13 +97,13 @@ def main():
     ]                 
     
     for name, version in plugins:
-        print '=' * 60
+        print( '=' * 60)
         basename = download_package(client, name, version)
-        print '-> downloaded', basename
+        print('-> downloaded', basename)
         directory = extract(basename)
-        print '-> extracted to', directory
+        print('-> extracted to', directory)
         result = run_tox(directory)
-        print '-> tox returned %s' % result 
+        print('-> tox returned %s' % result) 
     
 #===================================================================================================
 # main    
