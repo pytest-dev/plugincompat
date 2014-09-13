23957261440070343e883df2cce5e1aafb45e786
Bruno Oliveira <nicoddemus@gmail.com>
2014-09-13 12:39:16 -0300

    processing packages in parallel, added timeout when running tox

--- run.py
+++ run.py
@@ -15,16 +15,21 @@ Once all results are obtained, they are posted to the pytest-plugs heroku app
 which can then be visualized.
 """
 from __future__ import print_function, with_statement, division
-
+from contextlib import contextmanager
 import os
+import shutil
 import sys
 import tarfile
 from zipfile import ZipFile
 import subprocess
 import json
-
-import requests
+import traceback
+import io
 import time
+import threading
+
+import concurrent.futures
+import requests
 
 import update_index
 
@@ -37,13 +42,16 @@ else:
     from xmlrpclib import ServerProxy
 
 
+
+# oh my, urlretrieve is not thread safe :(
+_urlretrieve_lock = threading.Lock()
+
 def download_package(client, name, version):
-    found_dists = []
     for url_data in client.release_urls(name, version):
         basename = os.path.basename(url_data['url'])
-        found_dists.append(url_data['packagetype'])
         if url_data['packagetype'] == 'sdist':
-            urlretrieve(url_data['url'], basename)
+            with _urlretrieve_lock:
+                urlretrieve(url_data['url'], basename)
             return basename
 
     return None
@@ -60,7 +68,6 @@ def extract(basename):
     :return: the name of the directory where the contents where extracted
     """
     from contextlib import closing
-
 
     extractors = {
         '.zip': ZipFile,
@@ -123,34 +130,71 @@ def read_plugins_index(file_name):
         return json.load(f)
 
 
-def main():
-    pytest_version = os.environ['PYTEST_VERSION']
-    tox_env = 'py%d%d' % sys.version_info[:2]
-
+def process_package(tox_env, pytest_version, name, version, description):
+    def get_elapsed():
+        return time.time() - start
+    start = time.time()
     client = ServerProxy('https://pypi.python.org/pypi')
+    basename = download_package(client, name, version)
+    if basename is None:
+        result, output = 1, 'No sdist found'
+        return name, version, result, output, description, get_elapsed()
+    directory = extract(basename)
+    executor = concurrent.futures.ThreadPoolExecutor(max_workers=1)
+    f = executor.submit(run_tox, directory, tox_env, pytest_version)
+    try:
+        result, output = f.result(timeout=5 * 60)
+    except concurrent.futures.TimeoutError:
+        f.cancel()
+        result, output = 1, 'tox run timed out'
+    except Exception:
+        f.cancel()
+        stream = io.StringIO()
+        traceback.print_exc(file=stream)
+        result, output = 'error', 'traceback:\n%s' % stream.getvalue()
+    finally:
+        executor.shutdown(wait=False)
+    output += '\n\nTime: %.1f seconds' % get_elapsed()
+    return name, version, result, output, description, get_elapsed()
+
+
+@contextmanager
+def working_dir(new_cwd):
+    if os.path.isdir(new_cwd):
+        shutil.rmtree(new_cwd)
+    os.makedirs(new_cwd)
+    old_cwd = os.getcwd()
+    os.chdir(new_cwd)
+    yield new_cwd
+    os.chdir(old_cwd)
+
+
+def main():
+    #pytest_version = os.environ['PYTEST_VERSION']
+    pytest_version = '2.6.2'
+    tox_env = 'py%d%d' % sys.version_info[:2]
 
     plugins = read_plugins_index(update_index.INDEX_FILE_NAME)
 
     test_results = {}
     overall_start = time.time()
-    for plugin in plugins:
-        start = time.time()
-        name = plugin['name']
-        version = plugin['version']
-        description = plugin['description']
-        print('=' * 60)
-        print('%s-%s' % (name, version))
-        basename = download_package(client, name, version)
-        if basename is None:
-            print('-> No sdist found (skipping)')
-            continue
-        print('-> downloaded', basename)
-        directory = extract(basename)
-        print('-> extracted to', directory)
-        result, output = run_tox(directory, tox_env, pytest_version)
-        print('-> tox returned %s' % result)
-        print('-> time: %.1f seconds' % (time.time() - start))
-        test_results[(name, version)] = result, output, description
+    executor = concurrent.futures.ThreadPoolExecutor(max_workers=4)
+    with working_dir('.work'), executor:
+        fs = []
+        for plugin in plugins[:4]:
+            f = executor.submit(process_package, tox_env,
+                                pytest_version, plugin['name'],
+                                plugin['version'], plugin['description'])
+            fs.append(f)
+
+        print('Processing %d packages' % len(fs))
+        for f in concurrent.futures.as_completed(fs):
+            name, version, result, output, description, elapsed = f.result()
+            print('=' * 60)
+            print('%s-%s' % (name, version))
+            print('-> tox returned %s' % result)
+            print('-> time: %.1f seconds' % elapsed)
+            test_results[(name, version)] = result, output, description
 
     print('\n\n')
     overall_elapsed = time.time() - overall_start
@@ -191,4 +235,5 @@ def main():
 
 
 if __name__ == '__main__':
-    main()  
+    main()
+    os._exit(0) # futures may be still running, force exit
