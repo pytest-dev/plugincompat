653e0c8ed02a3327a9a17781e2c3d2862e886629
Bruno Oliveira <nicoddemus@gmail.com>
2015-12-15 20:08:41 -0200

    Update to 2.8.5

--- web.py
+++ web.py
@@ -249,7 +249,7 @@ def get_pytest_versions():
     """
     Same as `get_python_versions`, but for pytest versions.
     """
-    return {'2.7.3', '2.8.2'}
+    return {'2.7.3', '2.8.5'}
 
 # text returned when an entry in the database lacks an "output" field
 NO_OUTPUT_AVAILABLE = '<no output available>'
