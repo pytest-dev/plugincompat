fbd4dc8466d8d4f4c4d230849e6eea7a1bfd3371
Bruno Oliveira <nicoddemus@gmail.com>
2016-03-28 20:29:00 -0300

    Publish results for 2.8.7 and 2.9.1

--- test_web.py
+++ test_web.py
@@ -269,7 +269,7 @@ class TestView(object):
     def test_versions(self):
         from web import get_python_versions, get_pytest_versions
         assert get_python_versions() == {'py27', 'py35'}
-        assert get_pytest_versions() == {'2.7.3', '2.8.7'}
+        assert get_pytest_versions() == {'2.8.7', '2.9.1'}
 
 
     def test_get_with_empty_database(self, client, patched_storage):
