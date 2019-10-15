903d327661ef3b13a6967b2e3d547bdc042b0c0d
Bruno Oliveira <nicoddemus@gmail.com>
2019-10-15 20:59:38 -0300

    Run pre-commit in all files

--- run.py
+++ run.py
@@ -14,8 +14,6 @@ a dependency when invoking tox.
 Once all results are obtained, they are posted to the plugincompat heroku app
 which can then be visualized.
 """
-from __future__ import print_function, with_statement, division
-
 import concurrent.futures
 import json
 import os
@@ -30,7 +28,10 @@ from argparse import ArgumentParser
 from collections import defaultdict
 from collections import namedtuple
 from contextlib import closing
+from io import StringIO
 from tempfile import mkdtemp
+from urllib.request import urlretrieve
+from xmlrpc.client import ServerProxy
 from zipfile import ZipFile
 
 import colorama
@@ -45,11 +46,6 @@ from wimpy.util import working_directory
 import update_index
 
 
-from urllib.request import urlretrieve
-from xmlrpc.client import ServerProxy
-from io import StringIO
-
-
 # oh my, urlretrieve is not thread safe :(
 _urlretrieve_lock = threading.Lock()
 
@@ -58,24 +54,24 @@ def download_package(client, name, version):
     urls = client.release_urls(name, version)
     dists = defaultdict(list)
     for data in urls:
-        dists[data.get('packagetype')].append(data)
+        dists[data.get("packagetype")].append(data)
     url = fname = None
-    for sdist in dists['sdist']:
-        url = sdist['url']
-        fname = sdist['filename']
+    for sdist in dists["sdist"]:
+        url = sdist["url"]
+        fname = sdist["filename"]
         break
     else:
-        for bdist in dists['bdist_wheel']:
+        for bdist in dists["bdist_wheel"]:
             try:
-                if not is_compatible(bdist['filename']):
+                if not is_compatible(bdist["filename"]):
                     continue
             except distlib.DistlibException:
                 # is_compatible may also raise exceptions with invalid wheel
                 # files instead of returning False :/
                 continue
             else:
-                url = bdist['url']
-                fname = bdist['filename']
+                url = bdist["url"]
+                fname = bdist["filename"]
                 break
     if fname is not None:
         with _urlretrieve_lock:
@@ -94,17 +90,13 @@ def extract(basename):
     :return: the name of the directory where the contents where extracted
     """
 
-    extractors = {
-        '.zip': ZipFile,
-        '.tar.gz': tarfile.open,
-        '.tgz': tarfile.open,
-    }
+    extractors = {".zip": ZipFile, ".tar.gz": tarfile.open, ".tgz": tarfile.open}
     for ext, extractor in extractors.items():
         if basename.endswith(ext):
             with closing(extractor(basename)) as f:
-                f.extractall('.')
-            return basename[:-len(ext)]
-    raise Exception('could not extract %s' % basename)
+                f.extractall(".")
+            return basename[: -len(ext)]
+    raise Exception("could not extract %s" % basename)
 
 
 def run_tox(target, tox_env, pytest_version, mode="sdist"):
@@ -120,12 +112,12 @@ def run_tox(target, tox_env, pytest_version, mode="sdist"):
         PLACEHOLDER_TOX = PLACEHOLDER_TOX_BDIST.format(wheel_fname=target)
     else:
         raise NotImplementedError
-    tox_file = os.path.join(directory, 'tox.ini')
+    tox_file = os.path.join(directory, "tox.ini")
     if not os.path.isfile(tox_file):
-        with open(tox_file, 'w') as f:
+        with open(tox_file, "w") as f:
             f.write(PLACEHOLDER_TOX)
 
-    cmdline = 'tox --result-json=result.json -e %s --force-dep=pytest==%s'
+    cmdline = "tox --result-json=result.json -e %s --force-dep=pytest==%s"
     cmdline %= (tox_env, pytest_version)
     args = cmdline.split()
 
@@ -142,15 +134,15 @@ def run_tox(target, tox_env, pytest_version, mode="sdist"):
 
 # tox.ini contents when downloaded package does not have a tox.ini file
 # in this case we only display help information
-PLACEHOLDER_TOX_SDIST = '''\
+PLACEHOLDER_TOX_SDIST = """\
 [tox]
 
 [testenv]
 deps = pytest
 commands = pytest --trace-config --help
-'''
+"""
 
-PLACEHOLDER_TOX_BDIST = '''\
+PLACEHOLDER_TOX_BDIST = """\
 [tox]
 skipsdist = True
 
@@ -161,7 +153,7 @@ deps =
 commands =
     pip install ../{wheel_fname}
     pytest --trace-config --help
-'''
+"""
 
 
 def read_plugins_index(file_name):
@@ -169,7 +161,9 @@ def read_plugins_index(file_name):
         return json.load(f)
 
 
-PackageResult = namedtuple('PackageResult', 'name version status_code status output description elapsed')
+PackageResult = namedtuple(
+    "PackageResult", "name version status_code status output description elapsed"
+)
 
 
 def process_package(tox_env, pytest_version, name, version, description):
@@ -179,21 +173,21 @@ def process_package(tox_env, pytest_version, name, version, description):
     start = time.time()
 
     # if we already have results, skip testing this plugin
-    url = os.environ.get('PLUGINCOMPAT_SITE')
+    url = os.environ.get("PLUGINCOMPAT_SITE")
     if url:
         params = dict(py=tox_env, pytest=pytest_version)
-        response = requests.get('{}/output/{}-{}'.format(url, name, version), params=params)
+        response = requests.get("{}/output/{}-{}".format(url, name, version), params=params)
         if response.status_code == 200:
-            return PackageResult(name, version, 0, 'SKIPPED', 'Skipped', description,
-                                 get_elapsed())
+            return PackageResult(name, version, 0, "SKIPPED", "Skipped", description, get_elapsed())
 
-    client = ServerProxy('https://pypi.org/pypi')
+    client = ServerProxy("https://pypi.org/pypi")
     basename = download_package(client, name, version)
     if basename is None:
-        status_code, output = 1, 'No source or compatible distribution found'
-        return PackageResult(name, version, status_code, 'NO DIST', output, description,
-                             get_elapsed())
-    if basename.endswith('.whl'):
+        status_code, output = 1, "No source or compatible distribution found"
+        return PackageResult(
+            name, version, status_code, "NO DIST", output, description, get_elapsed()
+        )
+    if basename.endswith(".whl"):
         target = basename
         mode = "bdist_wheel"
     else:
@@ -205,16 +199,16 @@ def process_package(tox_env, pytest_version, name, version, description):
         status_code, output = f.result(timeout=5 * 60)
     except concurrent.futures.TimeoutError:
         f.cancel()
-        status_code, output = 1, 'tox run timed out'
+        status_code, output = 1, "tox run timed out"
     except Exception:
         f.cancel()
         stream = StringIO()
         traceback.print_exc(file=stream)
-        status_code, output = 1, 'traceback:\n%s' % stream.getvalue()
+        status_code, output = 1, "traceback:\n%s" % stream.getvalue()
     finally:
         executor.shutdown(wait=False)
-    output += '\n\nTime: %.1f seconds' % get_elapsed()
-    status = 'PASSED' if status_code == 0 else 'FAILED'
+    output += "\n\nTime: %.1f seconds" % get_elapsed()
+    status = "PASSED" if status_code == 0 else "FAILED"
     return PackageResult(name, version, status_code, status, output, description, get_elapsed())
 
 
@@ -223,82 +217,81 @@ def post_test_results(test_results, tox_env, pytest_version, secret):
     for (name, version) in sorted(test_results):
         result, output, description = test_results[(name, version)]
         if result == 0:
-            status = 'ok'
+            status = "ok"
         else:
-            status = 'fail'
+            status = "fail"
         results.append(
-            {'name': name,
-             'version': version,
-             'env': tox_env,
-             'pytest': pytest_version,
-             'status': status,
-             'output': output,
-             'description': description,
-             }
+            {
+                "name": name,
+                "version": version,
+                "env": tox_env,
+                "pytest": pytest_version,
+                "status": status,
+                "output": output,
+                "description": description,
+            }
         )
     if secret:
-        post_url = os.environ['PLUGINCOMPAT_SITE']
-        data = {
-            'secret': secret,
-            'results': results,
-        }
-        headers = {'content-type': 'application/json'}
-        response = requests.post(post_url, data=json.dumps(data),
-                                 headers=headers)
+        post_url = os.environ["PLUGINCOMPAT_SITE"]
+        data = {"secret": secret, "results": results}
+        headers = {"content-type": "application/json"}
+        response = requests.post(post_url, data=json.dumps(data), headers=headers)
         response.raise_for_status()
-        print(Fore.GREEN + 'Batch of {} posted'.format(len(test_results)))
+        print(Fore.GREEN + "Batch of {} posted".format(len(test_results)))
         return len(results)
     else:
-        msg = 'Skipping posting batch of {} because secret is not available'
+        msg = "Skipping posting batch of {} because secret is not available"
         print(Fore.YELLOW + msg.format(len(test_results)))
         return 0
 
 
 def printer(result_iterator, n_total):
     status_color_map = {
-        'SKIPPED': Fore.YELLOW,
-        'NO DIST': Fore.MAGENTA,
-        'PASSED': Fore.GREEN,
-        'FAILED': Fore.RED,
+        "SKIPPED": Fore.YELLOW,
+        "NO DIST": Fore.MAGENTA,
+        "PASSED": Fore.GREEN,
+        "FAILED": Fore.RED,
     }
     for i, package_result in enumerate(result_iterator, 1):
-        package = '%s-%s' % (package_result.name, package_result.version)
-        print('{package:<60s} {status_color}{package_result.status:>15s}'
-              '{elapsed_color}{package_result.elapsed:>6.1f}s '
-              '{percent_color}[%{percent:>3d}]'.format(
-            package=package,
-            status_color=status_color_map[package_result.status],
-            package_result=package_result,
-            elapsed_color=Fore.CYAN,
-            percent_color=Fore.LIGHTCYAN_EX,
-            percent=i * 100 // n_total,
-        ))
+        package = "{}-{}".format(package_result.name, package_result.version)
+        print(
+            "{package:<60s} {status_color}{package_result.status:>15s}"
+            "{elapsed_color}{package_result.elapsed:>6.1f}s "
+            "{percent_color}[%{percent:>3d}]".format(
+                package=package,
+                status_color=status_color_map[package_result.status],
+                package_result=package_result,
+                elapsed_color=Fore.CYAN,
+                percent_color=Fore.LIGHTCYAN_EX,
+                percent=i * 100 // n_total,
+            )
+        )
         yield package_result
 
 
 def main():
-    strip = False if 'TRAVIS' in os.environ else None
+    strip = False if "TRAVIS" in os.environ else None
     colorama.init(autoreset=True, strip=strip)
     parser = ArgumentParser()
-    parser.add_argument('--limit', type=int)
-    parser.add_argument('--workers', type=int, default=8)
-    parser.add_argument('--post-chunks', type=int, default=10)
+    parser.add_argument("--limit", type=int)
+    parser.add_argument("--workers", type=int, default=8)
+    parser.add_argument("--post-chunks", type=int, default=10)
 
     args = parser.parse_args()
     limit = args.limit
     workers = args.workers
     post_chunks = args.post_chunks
 
-    pytest_version = os.environ['PYTEST_VERSION']
+    pytest_version = os.environ["PYTEST_VERSION"]
 
     # important to remove POST_KEY from environment so others cannot sniff it somehow (#26)
-    secret = os.environ.pop('POST_KEY', None)
+    secret = os.environ.pop("POST_KEY", None)
     if secret is None and limit is None:
         # bail out early so CI doesn't take forever for a PR
         limit = args.post_chunks * 3
-        print(Fore.CYAN + 'Limit forced to {} since secret is unavail'.format(limit))
+        print(Fore.CYAN + "Limit forced to {} since secret is unavail".format(limit))
 
-    tox_env = 'py%d%d' % sys.version_info[:2]
+    tox_env = "py%d%d" % sys.version_info[:2]
 
     plugins = read_plugins_index(update_index.INDEX_FILE_NAME)
     if limit is not None:
@@ -312,12 +305,17 @@ def main():
     with working_directory(tmp), executor:
         fs = []
         for plugin in plugins:
-            f = executor.submit(process_package, tox_env,
-                                pytest_version, plugin['name'],
-                                plugin['version'], plugin['description'])
+            f = executor.submit(
+                process_package,
+                tox_env,
+                pytest_version,
+                plugin["name"],
+                plugin["version"],
+                plugin["description"],
+            )
             fs.append(f)
 
-        print(Fore.CYAN + 'Processing {} packages with {} workers'.format(len(fs), workers))
+        print(Fore.CYAN + "Processing {} packages with {} workers".format(len(fs), workers))
 
         results = (f.result() for f in concurrent.futures.as_completed(fs))
         results = printer(results, n_total=n_total)  # print them as they complete
@@ -325,19 +323,22 @@ def main():
         for chunk in chunked_results:
             test_results = {}
             for package_result in chunk:
-                if package_result.status != 'SKIPPED':
-                    test_results[(package_result.name, package_result.version)] = \
-                        package_result.status_code, package_result.output, package_result.description
-            n_posted += post_test_results(test_results, tox_env=tox_env,
-                                          pytest_version=pytest_version,
-                                          secret=secret)
+                if package_result.status != "SKIPPED":
+                    test_results[(package_result.name, package_result.version)] = (
+                        package_result.status_code,
+                        package_result.output,
+                        package_result.description,
+                    )
+            n_posted += post_test_results(
+                test_results, tox_env=tox_env, pytest_version=pytest_version, secret=secret
+            )
 
     print()
     if n_posted:
-        print(Fore.GREEN + 'Posted {} new results'.format(n_posted))
-    print(Fore.GREEN + 'All done, congratulations :)')
+        print(Fore.GREEN + "Posted {} new results".format(n_posted))
+    print(Fore.GREEN + "All done, congratulations :)")
     shutil.rmtree(tmp)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
