9fbd256c92a698d7c10dcbd96b2cd57aee2f2f55
Bruno Oliveira <nicoddemus@gmail.com>
2019-12-01 14:08:04 -0300

    Use trio in run.py
    Also changed requirements to include dev-requirements separately

--- test_run.py
+++ test_run.py
@@ -2,33 +2,41 @@ import json
 import os
 import sys
 import zipfile
+from pathlib import Path
 from shutil import copy
 from textwrap import dedent
 
+import asynctest
 import distlib
 import pytest
-import responses
-from requests.exceptions import HTTPError
 
 import run
 from run import download_package
 from run import extract
 from run import main
 from run import PackageResult
-from run import post_test_results
-from run import process_package
 from run import read_plugins_index
 
-canned_data = [
-    {"description": "the description 1", "name": "pytest-plugin-a", "version": "0.1.1"},
-    {"description": "the description 2", "name": "pytest-plugin-b", "version": "0.2.2"},
-    {"description": "the description 3", "name": "pytest-plugin-c", "version": "0.3.3"},
-]
 
-canned_results = {
-    # (name, version): (result, output, description),
-    ("pytest-shmytest", "0.0"): (0, "this one was ok", "a cool plugin"),
-    ("pytest-yo-dawg", "6.9"): (1, "this one was a failure", "uncool plugin"),
+packages_results = {
+    "pytest-plugin-a": PackageResult(
+        name="pytest-plugin-a",
+        version="0.1.1",
+        status_code=0,
+        status="PASSED",
+        output="whatever 1",
+        description="the description 1",
+        elapsed=0,
+    ),
+    "pytest-plugin-b": PackageResult(
+        name="pytest-plugin-b",
+        version="0.2.2",
+        status_code=1,
+        status="FAILED",
+        output="whatever 2",
+        description="the description 2",
+        elapsed=0,
+    ),
 }
 
 canned_tox_ini = """\
@@ -47,6 +55,11 @@ def patch_env(monkeypatch):
 
 @pytest.fixture(autouse=True)
 def fake_index_json(monkeypatch):
+    canned_data = [
+        {"description": "the description 1", "name": "pytest-plugin-a", "version": "0.1.1"},
+        {"description": "the description 2", "name": "pytest-plugin-b", "version": "0.2.2"},
+        {"description": "the description 3", "name": "pytest-plugin-c", "version": "0.3.3"},
+    ]
     monkeypatch.setattr("run.read_plugins_index", lambda file_name: canned_data)
 
 
@@ -64,7 +77,7 @@ def greyorama(monkeypatch):
     monkeypatch.setattr("run.Fore", Fore())
 
 
-def fake_process_package(tox_env, pytest_version, name, version, description):
+async def fake_run_package(session, tox_env, pytest_version, name, version, description):
     result = PackageResult(
         name=name,
         version=version,
@@ -77,26 +90,37 @@ def fake_process_package(tox_env, pytest_version, name, version, description):
     return result
 
 
-@pytest.fixture
-def posted_results(monkeypatch):
-    collector = []
-
-    def fake_post_test_results(test_results, tox_env, pytest_version, secret):
-        assert pytest_version == "1.2.3"
-        assert secret == "my cat's breath smells like cat food"
-        collector.append(test_results)
-        return len(test_results)
-
-    monkeypatch.setattr("run.post_test_results", fake_post_test_results)
-    return collector
+@pytest.fixture(name="mock_session")
+def mock_session_():
+    session = asynctest.MagicMock()
+    session.post = asynctest.CoroutineMock()
+    session.get = asynctest.CoroutineMock()
+    return session
 
 
-def test_main(monkeypatch, capsys, posted_results):
-    monkeypatch.setattr("run.process_package", fake_process_package)
+async def test_main(monkeypatch, capsys):
+    collected = []
+
+    class FakeResultsPoster:
+        def __init__(self, *args, **kwargs):
+            pass
+
+        async def maybe_post_batch(self, package_result):
+            collected.append(package_result)
+
+        async def post_all(self):
+            pass
+
+        @property
+        def total_posted(self):
+            return len(collected)
+
+    monkeypatch.setattr("run.ResultsPoster", FakeResultsPoster)
+    monkeypatch.setattr("run.run_package", fake_run_package)
     monkeypatch.setattr("sys.argv", ["run.py", "--limit=2", "--workers=1"])
     monkeypatch.setattr("colorama.init", lambda autoreset, strip: None)
     monkeypatch.setenv("POST_KEY", "my cat's breath smells like cat food")
-    main()
+    await main()
     out, err = capsys.readouterr()
     assert err == ""
     assert out == dedent(
@@ -109,74 +133,93 @@ def test_main(monkeypatch, capsys, posted_results):
         All done, congratulations :)
         """
     )
-    assert posted_results == [
-        {
-            ("pytest-plugin-a", "0.1.1"): (0, "whatever", "the description 1"),
-            ("pytest-plugin-b", "0.2.2"): (0, "whatever", "the description 2"),
-        }
+    assert collected == [
+        PackageResult(
+            name="pytest-plugin-a",
+            version="0.1.1",
+            status_code=0,
+            status="PASSED",
+            output="whatever",
+            description="the description 1",
+            elapsed=0,
+        ),
+        PackageResult(
+            name="pytest-plugin-b",
+            version="0.2.2",
+            status_code=0,
+            status="PASSED",
+            output="whatever",
+            description="the description 2",
+            elapsed=0,
+        ),
     ]
 
 
-@responses.activate
-def test_post_test_results(capsys):
-    responses.add(responses.POST, "http://plugincompat.example.com/")
-    post_test_results(canned_results, tox_env="py10", pytest_version="1.2.3", secret="ILIKETURTLES")
+async def test_post_test_results(capsys, mock_session):
+
+    poster = run.ResultsPoster(
+        mock_session, batch_size=2, tox_env="py38", pytest_version="3.5.2", secret="ILIKETURTLES"
+    )
+    await poster.maybe_post_batch(packages_results["pytest-plugin-a"])
+    assert mock_session.post.call_count == 0  # not posted yet
+
+    await poster.maybe_post_batch(packages_results["pytest-plugin-b"])
+    assert mock_session.post.call_count == 1
     out, err = capsys.readouterr()
     assert err == ""
     assert "Batch of 2 posted\n" in out
-    assert len(responses.calls) == 1
-    [call] = responses.calls
-    assert call.request.url == "http://plugincompat.example.com/"
-    assert json.loads(call.request.body) == {
+    assert mock_session.post.call_count == 1
+    args, kwargs = mock_session.post.call_args
+    assert args[0] == "http://plugincompat.example.com"
+    assert json.loads(kwargs["data"]) == {
         "results": [
             {
-                "description": "a cool plugin",
-                "env": "py10",
-                "name": "pytest-shmytest",
-                "output": "this one was ok",
-                "pytest": "1.2.3",
+                "description": "the description 1",
+                "env": "py38",
+                "name": "pytest-plugin-a",
+                "output": "whatever 1",
+                "pytest": "3.5.2",
                 "status": "ok",
-                "version": "0.0",
+                "version": "0.1.1",
             },
             {
-                "description": "uncool plugin",
-                "env": "py10",
-                "name": "pytest-yo-dawg",
-                "output": "this one was a failure",
-                "pytest": "1.2.3",
+                "description": "the description 2",
+                "env": "py38",
+                "name": "pytest-plugin-b",
+                "output": "whatever 2",
+                "pytest": "3.5.2",
                 "status": "fail",
-                "version": "6.9",
+                "version": "0.2.2",
             },
         ],
         "secret": "ILIKETURTLES",
     }
 
 
-@responses.activate
-def test_post_test_results_raises_for_status():
-    responses.add(responses.POST, "http://plugincompat.example.com", status=500)
-    error_message = "Internal Server Error for url: http://plugincompat.example.com"
-    with pytest.raises(HTTPError, match=error_message):
-        post_test_results(
-            canned_results, tox_env="py10", pytest_version="1.2.3", secret="ILIKETURTLES"
-        )
-
-
-def test_no_post_if_no_secret(capsys):
-    responses.add(responses.POST, "http://plugincompat.example.com", status=500)
-    post_test_results(canned_results, tox_env="py10", pytest_version="1.2.3", secret=None)
+async def test_no_post_if_no_secret(capsys, mock_session):
+    poster = run.ResultsPoster(
+        mock_session, batch_size=1, tox_env="py38", pytest_version="3.5.2", secret=None
+    )
+    await poster.maybe_post_batch(packages_results["pytest-plugin-a"])
     out, err = capsys.readouterr()
     assert err == ""
-    assert "Skipping posting batch of 2 because secret is not available" in out
+    assert "Skipping posting batch of 1 because secret is not available" in out
 
 
-@responses.activate
-def test_process_package_skips_if_result_already_on_plugincompat_website():
-    url = "http://plugincompat.example.com/output/myplugin-1.0?py=py10&pytest=1.2.3"
-    responses.add(responses.GET, url)
-    result = process_package(
-        tox_env="py10", pytest_version="1.2.3", name="myplugin", version="1.0", description="'sup"
+async def test_process_package_skips_if_result_already_on_plugincompat_website(mock_session):
+    mock_session.get.return_value.status_code = 200
+    result = await run.run_package(
+        session=mock_session,
+        tox_env="py10",
+        pytest_version="1.2.3",
+        name="myplugin",
+        version="1.0",
+        description="'sup",
     )
+    assert mock_session.get.call_count == 1
+    args, kwargs = mock_session.get.call_args
+    assert args[0] == "http://plugincompat.example.com/output/myplugin-1.0"
+    assert kwargs["params"] == dict(py="py10", pytest="1.2.3")
     assert result == PackageResult(
         name="myplugin",
         version="1.0",
@@ -188,14 +231,22 @@ def test_process_package_skips_if_result_already_on_plugincompat_website():
     )
 
 
-@responses.activate
-def test_process_package_no_dist_available(monkeypatch):
-    url = "http://plugincompat.example.com/output/myplugin-1.0?py=py10&pytest=1.2.3"
-    responses.add(responses.GET, url, status=404)
-    monkeypatch.setattr("run.download_package", lambda client, name, version: None)
-    result = process_package(
-        tox_env="py10", pytest_version="1.2.3", name="myplugin", version="1.0", description="'sup"
-    )
+async def test_process_package_no_dist_available(monkeypatch, mock_session):
+    mock_session.get.return_value.status_code = 404
+    with asynctest.patch("run.download_package", return_value=None, autospec=True):
+        result = await run.run_package(
+            mock_session,
+            tox_env="py10",
+            pytest_version="1.2.3",
+            name="myplugin",
+            version="1.0",
+            description="'sup",
+        )
+
+    assert mock_session.get.call_count == 1
+    args, kwargs = mock_session.get.call_args
+    assert args[0] == "http://plugincompat.example.com/output/myplugin-1.0"
+    assert kwargs["params"] == dict(py="py10", pytest="1.2.3")
     assert result == PackageResult(
         name="myplugin",
         version="1.0",
@@ -207,36 +258,46 @@ def test_process_package_no_dist_available(monkeypatch):
     )
 
 
-@responses.activate
-def test_process_package_tox_errored(tmpdir, monkeypatch):
-    url = "http://plugincompat.example.com/output/myplugin-1.0?py=py36&pytest=1.2.3"
-    responses.add(responses.GET, url, status=404)
-    monkeypatch.setattr("run.download_package", lambda client, name, version: "myplugin.zip")
+async def test_process_package_tox_errored(tmpdir, monkeypatch, mock_session):
+    mock_session.get.return_value.status_code = 404
     monkeypatch.chdir(tmpdir)
+
     tmpdir.join("myplugin").ensure_dir()
     tmpdir.join("myplugin").join("setup.py").ensure(file=True)
     with zipfile.ZipFile(str(tmpdir / "myplugin.zip"), mode="w") as z:
         z.write("myplugin")
-    result = process_package(
-        tox_env="py36", pytest_version="1.2.3", name="myplugin", version="1.0", description="'sup"
-    )
+
+    with asynctest.patch("run.download_package", return_value="myplugin.zip", autospec=True):
+        result = await run.run_package(
+            mock_session,
+            tox_env="py36",
+            pytest_version="1.2.3",
+            name="myplugin",
+            version="1.0",
+            description="'sup",
+        )
     assert result.name == "myplugin"
     assert result.status_code == 1
     assert result.status == "FAILED"
     assert "ERROR: setup.py is empty" in result.output
 
 
-@responses.activate
-def test_process_package_tox_crash(tmpdir, monkeypatch):
-    url = "http://plugincompat.example.com/output/myplugin-1.0?py=py36&pytest=1.2.3"
-    responses.add(responses.GET, url, status=404)
-    monkeypatch.setattr("run.download_package", lambda client, name, version: "myplugin.zip")
+async def test_process_package_tox_crash(tmpdir, monkeypatch, mock_session):
+    mock_session.get.return_value.status_code = 404
     monkeypatch.chdir(tmpdir)
+
     empty_zipfile_bytes = b"PK\x05\x06" + b"\x00" * 18
     tmpdir.join("myplugin.zip").write(empty_zipfile_bytes)
-    result = process_package(
-        tox_env="py36", pytest_version="1.2.3", name="myplugin", version="1.0", description="'sup"
-    )
+
+    with asynctest.patch("run.download_package", return_value="myplugin.zip", autospec=True):
+        result = await run.run_package(
+            mock_session,
+            tox_env="py36",
+            pytest_version="1.2.3",
+            name="myplugin",
+            version="1.0",
+            description="'sup",
+        )
     assert result.name == "myplugin"
     assert result.status_code == 1
     assert result.status == "FAILED"
@@ -245,12 +306,10 @@ def test_process_package_tox_crash(tmpdir, monkeypatch):
     assert "No such file or directory: {fn!r}".format(fn=fn) in result.output
 
 
-@responses.activate
-def test_process_package_tox_succeeded(tmpdir, monkeypatch):
+async def test_process_package_tox_succeeded(tmpdir, monkeypatch, mock_session):
     py = "py{}{}".format(*sys.version_info[:2])
-    url = "http://plugincompat.example.com/output/myplugin-1.0?py={}&pytest=3.7.4".format(py)
-    responses.add(responses.GET, url, status=404)
-    monkeypatch.setattr("run.download_package", lambda client, name, version: "myplugin.zip")
+    mock_session.get.return_value.status_code = 404
+
     monkeypatch.chdir(tmpdir)
     tmpdir.join("myplugin").ensure_dir()
     tmpdir.join("myplugin").join("setup.py").write(
@@ -259,9 +318,15 @@ def test_process_package_tox_succeeded(tmpdir, monkeypatch):
     tmpdir.join("myplugin").join("tox.ini").write(canned_tox_ini)
     with zipfile.ZipFile(str(tmpdir / "myplugin.zip"), mode="w") as z:
         z.write("myplugin")
-    result = process_package(
-        tox_env=py, pytest_version="3.7.4", name="myplugin", version="1.0", description="'sup"
-    )
+    with asynctest.patch("run.download_package", return_value="myplugin.zip", autospec=True):
+        result = await run.run_package(
+            mock_session,
+            tox_env=py,
+            pytest_version="3.7.4",
+            name="myplugin",
+            version="1.0",
+            description="'sup",
+        )
     assert result.name == "myplugin"
     assert result.version == "1.0"
     assert result.status_code == 0
@@ -284,12 +349,8 @@ def test_read_plugins(monkeypatch, tmpdir):
     assert result == {"k": "v"}
 
 
-def test_download_package(monkeypatch):
-    def fake_urlretrieve(url, basename):
-        assert url == "/path/to/whatever.tar.gz"
-        assert basename == "whatever.tar.gz"
-
-    monkeypatch.setattr("run.urlretrieve", fake_urlretrieve)
+async def test_download_package(mock_session):
+    mock_session.get.return_value.content = expected_content = b"some contents"
 
     class FakeClient:
         def release_urls(self, name, version):
@@ -301,16 +362,16 @@ def test_download_package(monkeypatch):
                 }
             ]
 
-    basename = download_package(client=FakeClient(), name="whatever", version="1.0")
+    basename = await download_package(
+        client=FakeClient(), session=mock_session, name="whatever", version="1.0"
+    )
+    assert mock_session.get.call_args[0][0] == "/path/to/whatever.tar.gz"
     assert basename == "whatever.tar.gz"
+    assert Path(basename).read_bytes() == expected_content
 
 
-def test_download_package_whl(monkeypatch, mocker):
-    def fake_urlretrieve(url, basename):
-        assert url == "/path/to/myplugin-1.0.0-py2.py3-none-any.whl"
-        assert basename == "myplugin-1.0.0-py2.py3-none-any.whl"
-
-    monkeypatch.setattr("run.urlretrieve", fake_urlretrieve)
+async def test_download_package_whl(monkeypatch, mocker, mock_session):
+    mock_session.get.return_value.content = b"some contents"
 
     m = mocker.patch.object(run, "is_compatible", autospec=True, return_value=True)
 
@@ -324,33 +385,48 @@ def test_download_package_whl(monkeypatch, mocker):
                 }
             ]
 
-    basename = download_package(client=FakeClient(), name="myplugin", version="1.0")
+    basename = await download_package(
+        session=mock_session, client=FakeClient(), name="myplugin", version="1.0"
+    )
     assert basename == "myplugin-1.0.0-py2.py3-none-any.whl"
 
     # incompatible wheel
     m.return_value = False
-    assert download_package(client=FakeClient(), name="myplugin", version="1.0") is None
+    assert (
+        await download_package(
+            session=mock_session, client=FakeClient(), name="myplugin", version="1.0"
+        )
+        is None
+    )
 
     # invalid wheel
     m.side_effect = distlib.DistlibException()
-    assert download_package(client=FakeClient(), name="myplugin", version="1.0") is None
+    assert (
+        await download_package(
+            session=mock_session, client=FakeClient(), name="myplugin", version="1.0"
+        )
+        is None
+    )
 
 
-@responses.activate
-def test_process_package_tox_succeeded_bdist(tmpdir, monkeypatch):
+async def test_process_package_tox_succeeded_bdist(datadir, monkeypatch, mock_session):
     py = "py{}{}".format(*sys.version_info[:2])
-    url = "http://plugincompat.example.com/output/myplugin-1.0.0?py={}&pytest=3.7.4".format(py)
-    responses.add(responses.GET, url, status=404)
-    monkeypatch.setattr(
-        "run.download_package", lambda client, name, version: "myplugin-1.0.0-py2.py3-none-any.whl"
-    )
-    here = os.path.dirname(__file__)
-    canned_whl = os.path.join(here, "test_data", "myplugin-1.0.0-py2.py3-none-any.whl")
-    copy(canned_whl, str(tmpdir))
-    monkeypatch.chdir(tmpdir)
-    result = process_package(
-        tox_env=py, pytest_version="3.7.4", name="myplugin", version="1.0.0", description="nope"
-    )
+    mock_session.get.return_value.status_code = 404
+
+    monkeypatch.chdir(datadir)
+
+    with asynctest.patch(
+        "run.download_package", return_value="myplugin-1.0.0-py2.py3-none-any.whl", autospec=True
+    ):
+        result = await run.run_package(
+            session=mock_session,
+            tox_env=py,
+            pytest_version="3.7.4",
+            name="myplugin",
+            version="1.0.0",
+            description="nope",
+        )
+    print(result.output)
     assert result.name == "myplugin"
     assert result.version == "1.0.0"
     assert result.status_code == 0
