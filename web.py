6ac0e244d5f4ba04aa8941366cfbd683f450e16a
Bruno Oliveira <nicoddemus@gmail.com>
2020-08-28 19:44:16 -0300

    Keep a single storage instance alive

--- web.py
+++ web.py
@@ -158,12 +158,18 @@ class PlugsStorage:
         return result
 
 
+_storage = None
+
+
 def get_storage_for_view():
     """
     Returns a storage instance to be used by the view functions. This exists
     solely we can mock this function during testing.
     """
-    return PlugsStorage()
+    global _storage
+    if _storage is None:
+        _storage = PlugsStorage()
+    return _storage
 
 
 def authenticate(json_data):
